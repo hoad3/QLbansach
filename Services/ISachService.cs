b36bef127b91c1aac1271eb80e1191a1f500b15f
milkyway <hoad3579@gmail.com>
2025-04-07 16:19:04 +0700

    commit lab 3

--- Services/ISachService.cs
+++ Services/ISachService.cs
@@ -5,4 +5,10 @@ namespace QLbansach_tutorial.Services;
 public interface ISachService
 {
     Task<List<Sach>> GetAllSachesAsync();
+    Task<List<Sach>> GetFirst4SachesAsync();
+    Task<List<Sach>> GetSachesByPageAsync(int page, int pageSize);
+    Task<int> GetTotalSachesCountAsync();
+    Task<(List<Sach> Saches, int TotalCount)> SearchSachesAsync(PayloadIndex model, int pageSize = 12);
+    Task<Sach> GetSachByIdAsync(int id);
+    Task<List<Sach>> GetRelatedSachesAsync(int maCD, int maNXB, int currentSachId, int limit = 4);
 }
